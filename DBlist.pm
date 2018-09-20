dc282d8d315ce3c5539858bda10a322adecbb1d2
meso_cacase <y-naito@dbcls.rois.ac.jp>
2018-09-20 09:17:28 +0900

    TogoGenome 89の試験用DBを追加
    - togogenome89 → 8分割されたインデックスを横断検索
    - togogenome89_big → 非分割の巨大インデックスを検索

--- DBlist.pm
+++ DBlist.pm
@@ -758,6 +758,7 @@ miRBase21mat             	172.18.8.72	43603	miRBase  	miRBase release 21, mature
 miRBase22                	172.18.8.72	43743	miRBase  	miRBase release 22, stem-loop microRNA sequences (Mar, 2018)	
 miRBase22mat             	172.18.8.72	43753	miRBase  	miRBase release 22, mature microRNA sequences (Mar, 2018)	
 togogenome88             	172.18.8.70	60383	         	TogoGenome from RefSeq 88 (May, 2018)	
+togogenome89_big         	172.18.8.70	60493	         	TogoGenome from RefSeq 89 (Jul, 2018)	
 togogenome89             	172.18.8.71	60483	         	TogoGenome from RefSeq 89 (Jul, 2018)	
 togogenome               	172.18.8.71	60483	         	TogoGenome from RefSeq 89 (Jul, 2018)	
 ddbj113hum               	172.18.8.77	58023	DDBJ     	DDBJ release 113.0, HUM division (Jun, 2018)	ddbjhum
