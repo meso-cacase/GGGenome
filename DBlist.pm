5b019b3382e838bdf2a26029f0fc72a7966222f2
meso_cacase <y-naito@dbcls.rois.ac.jp>
2020-07-30 13:04:48 +0900

    SARS-CoV-2ゲノムを更新(2020/7/29)

--- DBlist.pm
+++ DBlist.pm
@@ -863,7 +863,7 @@ ddbj119htc               	172.18.8.73	27877	DDBJ     	DDBJ release 119.0, HTC di
 ddbj119htg               	172.18.8.72	27878	DDBJ     	DDBJ release 119.0, HTG division (Mar, 2020)	ddbjhtg
 ddbj119sts               	172.18.8.78	27879	DDBJ     	DDBJ release 119.0, STS division (Mar, 2020)	ddbjsts
 ddbj119una               	172.18.8.73	27880	DDBJ     	DDBJ release 119.0, UNA division (Mar, 2020)	ddbjuna
-SARS-CoV-2               	172.18.8.76	55031	         	SARS-CoV-2 complete genomes, GenBank 2020/7/19	Severe acute respiratory syndrome coronavirus 2;COVID-19
+SARS-CoV-2               	172.18.8.76	55033	         	SARS-CoV-2 complete genomes, GenBank 2020/7/29	Severe acute respiratory syndrome coronavirus 2;COVID-19
 SARS-CoV-2-20200317      	172.18.8.76	55001	         	SARS-CoV-2 complete genomes, GenBank 2020/3/17	Severe acute respiratory syndrome coronavirus 2;COVID-19
 SARS-CoV-2-20200324      	172.18.8.76	55002	         	SARS-CoV-2 complete genomes, GenBank 2020/3/24	Severe acute respiratory syndrome coronavirus 2;COVID-19
 SARS-CoV-2-20200327      	172.18.8.76	55003	         	SARS-CoV-2 complete genomes, GenBank 2020/3/27	Severe acute respiratory syndrome coronavirus 2;COVID-19
@@ -895,6 +895,8 @@ SARS-CoV-2-20200704      	172.18.8.76	55028	         	SARS-CoV-2 complete genome
 SARS-CoV-2-20200709      	172.18.8.76	55029	         	SARS-CoV-2 complete genomes, GenBank 2020/7/9	Severe acute respiratory syndrome coronavirus 2;COVID-19
 SARS-CoV-2-20200714      	172.18.8.76	55030	         	SARS-CoV-2 complete genomes, GenBank 2020/7/14	Severe acute respiratory syndrome coronavirus 2;COVID-19
 SARS-CoV-2-20200719      	172.18.8.76	55031	         	SARS-CoV-2 complete genomes, GenBank 2020/7/19	Severe acute respiratory syndrome coronavirus 2;COVID-19
+SARS-CoV-2-20200724      	172.18.8.76	55032	         	SARS-CoV-2 complete genomes, GenBank 2020/7/24	Severe acute respiratory syndrome coronavirus 2;COVID-19
+SARS-CoV-2-20200729      	172.18.8.76	55033	         	SARS-CoV-2 complete genomes, GenBank 2020/7/29	Severe acute respiratory syndrome coronavirus 2;COVID-19
 COVID19-primercheck-EUL-20200501  	172.18.8.76	28831	NIHS	Cross-reactivity check for COVID-19 diagnostic primers (7 CoV + 32 organisms in WHO EUL), NIHS 2020/5/1	SARS-CoV-2
 COVID19-primercheck-EUA-20200501  	172.18.8.76	28832	NIHS	Cross-reactivity check for COVID-19 diagnostic primers (7 CoV + 20 organisms in US EUA), NIHS 2020/5/1	SARS-CoV-2
 COVID19-primercheck-CoV-20200501  	172.18.8.76	28833	NIHS	Cross-reactivity check for COVID-19 diagnostic primers (7 CoV), NIHS 2020/5/1	SARS-CoV-2
