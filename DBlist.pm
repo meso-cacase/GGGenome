b0dc4e335fce4703ef4655f308ffec61db3096f3
meso_cacase <y-naito@dbcls.rois.ac.jp>
2023-10-04 18:45:36 +0900

    RefSeq 220に更新

--- DBlist.pm
+++ DBlist.pm
@@ -749,7 +749,8 @@ refseq210                	172.18.8.70	28891	RefSeq   	RefSeq complete RNA releas
 refseq215                	172.18.8.73	28929	RefSeq   	RefSeq complete RNA release 215 (Nov, 2022)	
 refseq218                	172.18.8.137	27498	RefSeq   	RefSeq complete RNA release 218 (May, 2023)	
 refseq219                	172.18.8.138	27499	RefSeq   	RefSeq complete RNA release 219 (Jul, 2023)	
-refseq                   	172.18.8.138	27499	RefSeq   	RefSeq complete RNA release 219 (Jul, 2023)	
+refseq220                	172.18.8.135	28957	RefSeq   	RefSeq complete RNA release 220 (Sep, 2023)	
+refseq                   	172.18.8.135	28957	RefSeq   	RefSeq complete RNA release 220 (Sep, 2023)	
 hs_refseq                	172.18.8.75	28691	RefSeq   	RefSeq human RNA release 60 (Jul, 2013)	Homo sapiens
 mm_refseq                	172.18.8.75	28692	RefSeq   	RefSeq mouse RNA release 60 (Jul, 2013)	Mus musculus
 hg19_pre_mRNA            	172.18.8.75	28693	         	Human pre mRNA, Bits DB (Dec, 2014)	Homo sapiens
